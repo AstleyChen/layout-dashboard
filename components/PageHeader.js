ce0e5f7afbc14d14daf65c7cf15b3f3d443a8be0
Astley Chen <aschen@mozilla.com>
2016-06-28 23:05:05 +0800

    Rename dashboard name

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -3,7 +3,7 @@ import NetworkStatus from './NetworkStatus';
 
 const PageHeader = (props) => (
   <div id="header">
-    <h1>Mozilla DevRel Dashboard</h1>
+    <h1>Mozilla Taipei Layout Dashboard</h1>
     <NetworkStatus />
   </div>
 );
